d5bd9c3260baa8e49a7dc6935f993934a62b5989
Mike Mitterer <office@mikemitterer.at>
2016-12-31 00:39:10 +0100

    Thread-Lib wird jetzt bei Linux als cmake_cxx_flags gesetzt

--- Config.cmake
+++ Config.cmake
@@ -13,6 +13,10 @@ add_compile_options("-Wall")
 
 # GNU - gcc
 if (${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU" )
+
+endif ()
+
+if (${CMAKE_SYSTEM_NAME} STREQUAL "Linux" )
     # Wichtig - sonst funktioniert's auf dem Raspi (und auf Linux) nicht
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pthread")
 endif ()
